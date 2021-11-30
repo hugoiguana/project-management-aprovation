397fa04ef7f70490b5c486c1c7b78050bbd347ee
Hugo Mota <hugo.mota@ambisig.com>
2021-11-30 11:47:37 -0300

    First Configuration

---
+++ src/main/java/com/hugo/mota/projectmanagementaprovation/ProjectManagementAprovationApplication.java
@@ -0,0 +1,13 @@
+package com.hugo.mota.projectmanagementaprovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementAprovationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementAprovationApplication.class, args);
+	}
+
+}
