44e2741b4cd35884cc7d33d89d7834929ff0a2df
Hugo Mota <hugo.mota@ambisig.com>
2021-11-30 11:47:37 -0300

    First Configuration

---
+++ src/test/java/com/hugo/mota/projectmanagementaprovation/ProjectManagementAprovationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.mota.projectmanagementaprovation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementAprovationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
